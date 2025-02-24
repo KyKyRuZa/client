331b187d606376bf7cb0e18f761615b82ad0189a
KyKyRuZa <ituhvatullin8@gmail.com>
2025-02-24 19:43:43 +0300

    we

--- src/api/profile.js
+++ src/api/profile.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://213.108.198.68/api';
+const API_URL = 'http://213.108.198.68:5000/api';
 
 const profileService = {
     async fetchProducts() {
