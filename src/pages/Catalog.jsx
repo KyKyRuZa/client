ef6f603451006a0cf955101861d5f3055c1e4abd
KyKyRuZa <ituhvatullin8@gmail.com>
2025-03-10 20:02:17 +0300

    upd3

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -52,7 +52,7 @@ const Catalog = () => {
                   className="card-img"
                   style={{
                     backgroundImage: product.imageUrl
-                      ? `url(http://localhost:5000${product.imageUrl})`
+                      ? `url(https://delron.ru${product.imageUrl})`
                       : "url(https://via.placeholder.com/150)",
                     backgroundSize: "cover",
                     backgroundPosition: "center",
