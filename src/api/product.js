d54cf33f96ea1c299e12ca61e01a137b7d5815e4
KyKyRuZa <ituhvatullin8@gmail.com>
2025-03-16 21:33:43 +0300

    fix

--- src/api/product.js
+++ src/api/product.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/profile';
+const API_URL = 'https://delron.ru/api/profile';
 
 const productService = {
     // Операции с продуктом
